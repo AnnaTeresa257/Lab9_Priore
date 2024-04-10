2f806d56333701d463579618afbf43e9a98a0f86
AnnaTeresa257 <annateresapriore@gmail.com>
2024-04-10 10:36:49 -0400

    main commit 3

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import sys
+
+
+def print_menu():
+    print("Menu"
+          "\n -------------"
+          "\n 1. Encode"
+          "\n 2. Decode"
+          "\n 3. Quit"
+          "\n")
+
+
 def encode(password):
     new_list = []
     for i in password:
@@ -6,3 +18,19 @@ def encode(password):
     return new_list
 
 
+def main():
+    print_menu()
+    menu_option = int(input("Please enter an option: "))
+    if menu_option == "1":
+        password = input("Please enter a password to encode: ")
+        encoded = encode(password)
+        print_menu()
+    if menu_option == "2":
+        print(f"The encoded password is {encoded}, and the original password is {password}.")
+        print_menu()
+    if menu_option == "3":
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
