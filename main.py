ba33c85a7ca9402ca2abf8a81882d6808c02e00c
AnnaTeresa257 <annateresapriore@gmail.com>
2024-04-10 10:43:20 -0400

    hopefully last commit

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 
 def print_menu():
     print("Menu"
-          "\n -------------"
-          "\n 1. Encode"
-          "\n 2. Decode"
-          "\n 3. Quit"
+          "\n-------------"
+          "\n1. Encode"
+          "\n2. Decode"
+          "\n3. Quit"
           "\n")
 
 
@@ -19,17 +19,20 @@ def encode(password):
 
 
 def main():
-    print_menu()
-    menu_option = int(input("Please enter an option: "))
-    if menu_option == "1":
-        password = input("Please enter a password to encode: ")
-        encoded = encode(password)
+    while True:
         print_menu()
-    if menu_option == "2":
-        print(f"The encoded password is {encoded}, and the original password is {password}.")
-        print_menu()
-    if menu_option == "3":
-        sys.exit()
+        menu_option = input("Please enter an option: ")
+        if menu_option == "3":
+            sys.exit()
+
+        if menu_option == "1":
+            password = input("Please enter a password to encode: ")
+            encoded = encode(password)
+            print("You password has been encoded and stored!")
+            print()
+        if menu_option == "2":
+            print(f"The encoded password is {encoded}, and the original password is {password}.")
+            print()
 
 
 if __name__ == "__main__":
